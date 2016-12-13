fd10b4e6f86f879703c127f573e5bdd83586321d
shangxinbo <314911714@qq.com>
2016-12-13 11:31:31 +0800

    no message

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,28 @@
 
 const gulp = require('gulp');
-const replace = require('gulp-replace');
-const md  = require('markdown');
+var markdown = require('gulp-markdown');
+var header = require('gulp-header');
+var footer = require('gulp-footer');
+
+var template ={
+    header:'<!DOCTYPE html>\
+            <html lang="en">\
+            <head>\
+                <meta charset="UTF-8">\
+                <title>ShangXinbo——A simple web front engineer</title>\
+                <meta name="viewport" content="width=device-width, initial-scale=1">\
+                <link href="../css/github-markdown.css" rel="stylesheet" />\
+                <link href="../css/article.css" rel="stylesheet" />\
+            </head>\
+            <body class="markdown-body">',
+    footer:'</body></html>'
+};
 
 
 gulp.task('blog',function(){
     gulp.src('md/*.md')
-        /*.pipe(function(source){
-            return md.markdown.toHtml(source);
-        })*/
-        .pipe(gulp.dest('blog/*.html'));
+        .pipe(markdown())
+        .pipe(header(template.header))
+        .pipe(footer(template.footer))
+        .pipe(gulp.dest('blog'));
 });
--- index.js
+++ index.js
@@ -12,6 +12,21 @@ fs.readFile('md/mylife.md', function (err, data) {
         return console.error(err);
     }
     let mdString = md.markdown.toHTML(data.toString(),'Maruku');
-    console.log(mdString);
+    let html = template(mdString);
+    console.log(html);
 });
 
+
+function template(html){
+    return '<!DOCTYPE html>\
+            <html lang="en">\
+            <head>\
+                <meta charset="UTF-8">\
+                <title>ShangXinbo——A simple web front engineer</title>\
+                <meta name="viewport" content="width=device-width, initial-scale=1">\
+                <link href="css/pure-min.css" rel="stylesheet" />\
+                <link href="css/grids-responsive-min.css" rel="stylesheet" />\
+                <link href="css/index.css" rel="stylesheet" />\
+            </head>\
+            <body>' + html+ '</body></html>';
+}
