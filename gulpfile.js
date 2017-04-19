9114e965a2949d6321ff9d522f69737914996de7
shangxinbo <314911714@qq.com>
2017-04-19 16:37:10 +0800

    update npm and env

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,12 @@
-"use strict";
-const gulp = require('gulp');
-const replace = require('gulp-replace');
-const markdown = require('gulp-markdown');
-const header = require('gulp-header');
-const footer = require('gulp-footer');
-const glob = require("glob");
-const fs = require('fs');
-const md = require('markdown');
+"use strict"
+const gulp = require('gulp')
+const replace = require('gulp-replace')
+const markdown = require('gulp-markdown')
+const header = require('gulp-header')
+const footer = require('gulp-footer')
+const glob = require("glob")
+const fs = require('fs')
+const md = require('markdown')
 
 let articleTemplate = {
     header: '<!DOCTYPE html>\
@@ -19,43 +19,43 @@ let articleTemplate = {
             </head>\
             <body class="markdown-body">',
     footer: '</body></html>'
-};
+}
 
 gulp.task('blog', function () {        // md ==> html
     gulp.src('md/*.md')
         .pipe(markdown())
         .pipe(header(articleTemplate.header))
         .pipe(footer(articleTemplate.footer))
-        .pipe(gulp.dest('blog'));
-});
+        .pipe(gulp.dest('blog'))
+})
 
 gulp.task('list', function () {        // update diary.html
     let arr = new Array();
     let table = '';
     glob('md/*.md', function (err, files) {
         for (let i = 0; i < files.length; i++) {
-            let file = fs.readFileSync(files[i]);
-            let mdString = md.markdown.toHTMLTree(file.toString(), 'Maruku');
+            let file = fs.readFileSync(files[i])
+            let mdString = md.markdown.toHTMLTree(file.toString(), 'Maruku')
             arr.push({
                 time: mdString[2][1][1],
                 title: mdString[1][1],
                 href: files[i].replace('md/', 'blog/').replace('.md', '.html')
-            });
+            })
         }
         arr.sort(function (a, b) {     //sort by time
             return a.time < b.time
-        });
+        })
         for (let j = 0; j < arr.length; j++) {
-            table += '<tr><td><a href="' + arr[j]['href'] + '">' + arr[j]['title'] + '</a></td><td>' + arr[j]['time'] + '</td></tr>';
+            table += '<tr><td><a href="' + arr[j]['href'] + '">' + arr[j]['title'] + '</a></td><td>' + arr[j]['time'] + '</td></tr>'
         }
         gulp.src('diary.html')
             .pipe(replace(/<table class="list">([\s\S]*)<\/table>/, '<table class="list">' + table + '</table>'))   //update list
             .pipe(gulp.dest('.'));     //release origin dir
-    });
-});
+    })
+})
 
 gulp.task('watch', function () {       // develop
-    gulp.watch('md/*.md', ['blog', 'list']);
-});
+    gulp.watch('md/*.md', ['blog', 'list'])
+})
 
-gulp.task('default', ['blog', 'list']);
+gulp.task('default', ['blog', 'list'])
